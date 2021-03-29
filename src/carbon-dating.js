ebe0e2619f890f61c9ae223df2f7b37fa11fc20c
AntonVoinilovich <71197218+AntonVoinilovich@users.noreply.github.com>
2021-03-30 01:40:20 +0300

    fix: adding a task manually
    fix: adding a task manually

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,20 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) != 'string'){
+    return false;
+}
+if(isNaN(parseFloat(sampleActivity))){
+    return false;
+}
+
+let sampleActivityNumber = parseFloat(sampleActivity);
+
+if(sampleActivityNumber >= MODERN_ACTIVITY || sampleActivityNumber <= 0){
+    return false;
+}else{
+    let age = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivityNumber)/(0.693/HALF_LIFE_PERIOD));
+return age;
+}
 };
